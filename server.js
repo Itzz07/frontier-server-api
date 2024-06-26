8361e728a68a0522adc1d1249148bc8aa302028e
itzz07 <zimbajoel1@gmail.com>
2024-06-26 13:26:16 +0200

    commit

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 require("dotenv").config(); // Load environment variables from .env file
-
+// const serviceAccount = require("./serviceAccountKey.json");
 const admin = require("firebase-admin");
 // const serviceAccount = require("./serviceAccountKey.json");
-// const privateKey = process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n');
-// const serviceAccount = {
-//   type: process.env.GOOGLE_TYPE,
-//   project_id: process.env.GOOGLE_PROJECT_ID,
-//   private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-//   private_key: privateKey,
-//   client_email: process.env.GOOGLE_CLIENT_EMAIL,
-//   client_id: process.env.GOOGLE_CLIENT_ID,
-//   auth_uri: process.env.GOOGLE_AUTH_URI,
-//   token_uri: process.env.GOOGLE_TOKEN_URI,
-//   auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
-//   client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
-//   universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
-// };
+const privateKey = process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n');
+const serviceAccount = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: privateKey,
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+};
 // const { startScheduler } = require('./scheduler');
 const express = require("express");
 const cors = require("cors");
